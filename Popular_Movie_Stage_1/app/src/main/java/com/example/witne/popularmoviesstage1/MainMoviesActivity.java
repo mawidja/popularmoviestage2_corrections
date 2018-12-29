db254912548cd507993e22aaf63cea2659db20e1
Witness Tetani <witness.tetani@gmail.com>
2018-12-29 22:58:25 +0200

    Initial Devepolment of Popular Movies Stage 1 Application

---
+++ Popular_Movie_Stage_1/app/src/main/java/com/example/witne/popularmoviesstage1/MainMoviesActivity.java
@@ -0,0 +1,13 @@
+package com.example.witne.popularmoviesstage1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_movies);
+    }
+}
